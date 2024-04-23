73e9d75dc60ce5eba520bfd59ef2186958bad62f
Sergio Garcia <sergiog@caixabanktech.com>
2024-04-23 16:38:11 +0200

    [ADD] Initial project commit

---
+++ ApexLibrary/ApexLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApexLibraryApp.swift
+//  ApexLibrary
+//
+//  Created by Sergio Garcia on 23/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ApexLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApexLibrary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ApexLibrary
+//
+//  Created by Sergio Garcia on 23/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
