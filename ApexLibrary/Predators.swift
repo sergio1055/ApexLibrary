e2fa9461dcd1eb0da13df0f0f10f30885bae35f3
Sergio Garcia <sergiog@caixabanktech.com>
2024-04-27 12:53:30 +0200

    ADD: Data manipulation class and Apex model

---
+++ ApexLibrary/Predators.swift
@@ -0,0 +1,51 @@
+//
+//  Preadators.swift
+//  ApexLibrary
+//
+//  Created by Sergio Garcia on 24/4/24.
+//
+
+import Foundation
+ 
+class Predators {
+    var apexPredators: [ApexPredator] = []
+    
+    init() {
+        decodeApexPredatorData()
+    }
+    
+    func decodeApexPredatorData() {
+        if let url = Bundle.main.url(forResource: "jpapexpredators", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                apexPredators = try decoder.decode([ApexPredator].self, from: data)
+            } catch {
+                print("Error decoding JSON daata")
+            }
+        }
+    }
+    
+    func search(for searchTerm: String) -> [ApexPredator] {
+        if searchTerm.isEmpty {
+            return apexPredators
+        } else {
+            return apexPredators.filter {
+                predator in
+                predator.name.localizedCaseInsensitiveContains(searchTerm)
+            }
+        }
+    }
+    
+    func sort(by alphabetical: Bool) {
+        apexPredators.sort {
+            predator1, predator2 in
+            if alphabetical {
+                predator1.name < predator2.name
+            } else {
+                predator1.id < predator2.id
+            }
+        }
+    }
+}
